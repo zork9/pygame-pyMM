528e0309d8cc20e03d115613842a9f2d7ca5e49c
U-bubbletown\bubble <bubble@bubbletown.(none)>
2013-10-19 11:01:43 +0200

    license added

--- greenscorpion.py
+++ greenscorpion.py
@@ -1,4 +1,20 @@
 
+
+# Copyright (c) 2013 Johan Ceuppens.
+# All rights reserved.
+
+# Redistribution and use in source and binary forms are permitted
+# provided that the above copyright notice and this paragraph are
+# duplicated in all such forms and that any documentation,
+# advertising materials, and other materials related to such
+# distribution and use acknowledge that the software was developed
+# by the Johan Ceuppens.  The name of the
+# Johan Ceuppens may not be used to endorse or promote products derived
+# from this software without specific prior written permission.
+# THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
+# IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+
 # Copyright (C) Johan Ceuppens 2010
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
